80c449a98ca8a3ac0756aef1b8afea84a7254687
Steven C. Buttgereit <scb@musesystems.com>
2017-04-26 07:25:27 -0700

    Add trigger managing entity system lock flag
    When there are no more managing packages for a system locked entity in
    the entity_package table, remove the system locked restriction from the
    entity in the entity table.  This effectively turns the entity into a
    user managed record which would be consistent with the possible desire
    to use the entity outside of the package context.

---
+++ database/triggers/trig_a_d_entity_package_manage_entity_syslock.sql
@@ -0,0 +1,60 @@
+/*************************************************************************
+ *************************************************************************
+ **
+ ** File:         trig_a_d_entity_package_manage_entity_syslock.sql
+ ** Project:      Muse Systems xTuple Super Characteristics
+ ** Author:       Steven C. Buttgereit
+ **
+ ** (C) 2017 Lima Buttgereit Holdings LLC d/b/a Muse Systems
+ **
+ ** Contact:
+ ** muse.information@musesystems.com  :: https://muse.systems
+ ** 
+ ** License: MIT License. See LICENSE.md for complete licensing details.
+ **
+ *************************************************************************
+ ************************************************************************/
+
+--
+-- When the last managing package is deleted, set the related entity record to
+-- being not system locked.
+--
+
+CREATE OR REPLACE FUNCTION musesuperchar.trig_a_d_entity_package_manage_entity_syslock() 
+    RETURNS trigger AS
+        $BODY$
+            DECLARE
+                
+            BEGIN
+
+                IF NOT EXISTS (SELECT   true
+                                FROM    musesuperchar.entity_package 
+                                WHERE   entity_package_entity_id = 
+                                            OLD.entity_package_entity_id
+                                    AND entity_package_id != 
+                                            OLD entity_package_id) THEN
+                    -- If we don't have any more managing packages, set the 
+                    -- system lock to be not locked so the user can manage.
+                    UPDATE musesuperchar.entity
+                        SET entity_is_system_locked = false
+                        WHERE entity_is_system_locked
+                            AND entity_id = OLD.entity_package_entity_id;
+                
+                END IF;
+
+                RETURN OLD;
+
+            END;
+        $BODY$
+    LANGUAGE plpgsql VOLATILE;
+
+ALTER FUNCTION musesuperchar.trig_a_d_entity_package_manage_entity_syslock()
+    OWNER TO admin;
+
+REVOKE EXECUTE ON FUNCTION musesuperchar.trig_a_d_entity_package_manage_entity_syslock() FROM public;
+GRANT EXECUTE ON FUNCTION musesuperchar.trig_a_d_entity_package_manage_entity_syslock() TO admin;
+GRANT EXECUTE ON FUNCTION musesuperchar.trig_a_d_entity_package_manage_entity_syslock() TO xtrole;
+
+
+COMMENT ON FUNCTION musesuperchar.trig_a_d_entity_package_manage_entity_syslock() 
+    IS $DOC$When the last managing package is deleted, set the related entity record to being not system locked.$DOC$;
