93cb3a3211dec09b17d425aa800da349def54cf0
Steven C. Buttgereit <scb@musesystems.com>
2018-07-17 07:58:01 -0700

    Refactor extraneous whitespace

--- database/views/v_form_builder_widgets.init.sql
+++ database/views/v_form_builder_widgets.init.sql
@@ -9,7 +9,7 @@
  **
  ** Contact:
  ** muse.information@musesystems.com  :: https://muse.systems
- ** 
+ **
  ** License: MIT License. See LICENSE.md for complete licensing details.
  **
  *************************************************************************
@@ -21,7 +21,7 @@
 
 CREATE OR REPLACE VIEW musesuperchar.v_form_builder_widgets AS
     SELECT   scdef_scgrp_ass_id
-            ,scgrp_id 
+            ,scgrp_id
             ,scgrp_internal_name
             ,scgrp_display_name
             ,scdef_id
@@ -36,13 +36,13 @@ CREATE OR REPLACE VIEW musesuperchar.v_form_builder_widgets AS
             ,max(current_column::integer) OVER (PARTITION BY scdef_scgrp_ass_section_name) + 1 AS section_column_count
             ,section_internal_name||'_column_'||current_column AS internal_column_name
             ,current_column::integer AS column_number
-            ,row_number() 
-                OVER 
-                    (PARTITION BY section_internal_name||'_column_'||current_column 
+            ,row_number()
+                OVER
+                    (PARTITION BY section_internal_name||'_column_'||current_column
                         ORDER BY section_internal_name||'_column_'||current_column, scdef_scgrp_ass_sequence) - 1 AS row_number
     FROM
         (SELECT   scdef_scgrp_ass_id
-                    ,scgrp_id 
+                    ,scgrp_id
                     ,scgrp_internal_name
                     ,scgrp_display_name
                     ,scdef_id
@@ -55,16 +55,16 @@ CREATE OR REPLACE VIEW musesuperchar.v_form_builder_widgets AS
                     ,scdef_scgrp_ass_section_name
                     ,lower(regexp_replace(regexp_replace(scdef_scgrp_ass_section_name,E'[^[:alnum:]_]+','_','g'),E'^_+|_+$','','g')) AS section_internal_name
                     ,substring('00'||sum(CASE WHEN scdef_scgrp_ass_is_column_start THEN 1 ELSE 0 END)
-                                OVER (PARTITION BY scdef_scgrp_ass_section_name 
+                                OVER (PARTITION BY scdef_scgrp_ass_section_name
                                         ORDER BY scdef_scgrp_ass_section_name
                                                 ,scdef_scgrp_ass_sequence),E'..$') AS current_column
                     ,scdef_scgrp_ass_sequence
             FROM     musesuperchar.scdef_scgrp_ass
-                JOIN musesuperchar.scgrp 
+                JOIN musesuperchar.scgrp
                     ON scdef_scgrp_ass_scgrp_id = scgrp_id
-                JOIN musesuperchar.scdef 
+                JOIN musesuperchar.scdef
                     ON scdef_scgrp_ass_scdef_id = scdef_id
-                JOIN musesuperchar.datatype 
+                JOIN musesuperchar.datatype
                     ON scdef_datatype_id = datatype_id
             ORDER BY scdef_scgrp_ass_section_name
                     ,scdef_scgrp_ass_sequence) q;
@@ -75,5 +75,5 @@ REVOKE ALL ON TABLE musesuperchar.v_form_builder_widgets FROM public;
 GRANT ALL ON TABLE musesuperchar.v_form_builder_widgets TO admin;
 GRANT ALL ON TABLE musesuperchar.v_form_builder_widgets TO xtrole;
 
-COMMENT ON VIEW musesuperchar.v_form_builder_widgets 
+COMMENT ON VIEW musesuperchar.v_form_builder_widgets
     IS $DOC$Assembles the basic elements required for building the Qt .ui XML files.  We go so far as including the fundamental widget xml and names required for constructing subsequent levels of the format.$DOC$;
